e214aeb3dfd28d5d2222f25fa95a09fd847b9a4a
Farhan Ali <fahanali3193@gmail.com>
2021-09-01 23:22:07 -0700

    Added eslint, .babelrc, enzyme, test folder, sample test

---
+++ test/app/app.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../client/src/components/app.jsx';
+
+test('checks that app component is rendering', () => {
+  const wrapper = shallow(<App />, {disableLifecycleMethods: true})
+  expect(wrapper.children()).toHaveLength(1);
+});
