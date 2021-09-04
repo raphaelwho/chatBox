b88c76e00802d1be2d077070254a68753bd446d7
Farhan Ali <fahanali3193@gmail.com>
2021-09-04 13:58:00 -0700

    Initialized db

---
+++ db/index.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg');
+
+let database;
+let user;
+let password;
+let host;
+
+if (process.env.NODE_ENV === 'development') {
+  database = 'test5';
+  user = process.env.PG__DB_USER_DEV;
+  password = process.env.PG_DB_PASS_DEV;
+  host = 'localhost';
+} else if (process.env.NODE_ENV === 'test') {
+  database = 'postgres';
+  user = 'postgres';
+  password = process.env.PG_DB_PASS_TEST;
+  host = 'localhost';
+}
+
+const client = new Client({
+  database: database,
+  user: user,
+  password: password,
+  host: host,
+  port: 5432,
+});
+console.log('DB env', process.env.NODE_ENV)
+
+client.connect()
+  .then((client) => {
+    console.log(`CONNECTED TO PG: DATABASE: ${database}`);
+  })
+  .catch((err) => console.log(`ERROR CONNECTING TO PG: DATABASE ${database}`, err))
+
+
+module.exports.client = client;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./routes/routes.js');
+const db = require('../db/index.js');
 
 const servingPath = path.join(__dirname, '..', 'client', 'dist');
 app.use(express.static(servingPath));
