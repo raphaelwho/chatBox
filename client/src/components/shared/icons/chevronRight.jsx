53a058139e3942e417f8d48c94d05f8232eed1fb
Farhan Ali <fahanali3193@gmail.com>
2021-09-04 23:57:24 -0700

    Added trash and chevron right icons. Passing in all the required props

--- client/src/components/shared/icons/chevronRight.jsx
+++ client/src/components/shared/icons/chevronRight.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const ChevronRight = ({size=24, color="#000000"}) => (
+const ChevronRight = ({ size=24, color="#000000", onIconClick }) => (
   <svg
     className={`right-arrow`}
-    // onClick={nextImage}
+    onClick={onIconClick}
     xmlns="http://www.w3.org/2000/svg"
     width={size}
     height={size}
     viewBox="0 0 24 24"
     fill="none"
     stroke={color}
-    strokeWidth="2"
+    strokeWidth="1"
     strokeLinecap="round"
     strokeLinejoin="round">
     <path d="M9 18l6-6-6-6"/>
--- client/src/components/shared/parkingSpot/parkingSpot.jsx
+++ client/src/components/shared/parkingSpot/parkingSpot.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import ChevronRight from '../icons/chevronRight.jsx';
 import './parkingSpot.css';
 
-class ParkingSpot extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {};
-  }
-
-  render () {
-    return (
-      <div className='spot'>
-        <div className='spot-photo'>
-          PHOTO
-        </div>
-        <div className='spot-info'>
-          <p> E 60th Hyatt Place </p>
-          <p> New York, NY 1005 </p>
-          <p className='rate'>$2/hr</p>
-        </div>
-        <div className='spot-arrow'>
-          <ChevronRight size={100} color={'#707070'}/>
-        </div>
+let ParkingSpot = ({ image, rate, firstLine, secondLine, date, time, total, onIconClick, ActionIcon }) => {
+  return (
+    <div className='spot'>
+      <div className='spot-photo'>
+        <img src={image}/>
       </div>
-    )
-  };
+
+      <div className='spot-info'>
+        <p> {firstLine} </p>
+        <p> {secondLine} </p>
+        <p className='rate'> {date} </p>
+        <p className='rate'> {time} </p>
+        <p className='rate'> {total} </p>
+        <p className='rate'>{rate}</p>
+      </div>
+
+      <div className='spot-arrow'>
+        <ActionIcon size={60} color={'#707070'} onIconClick={onIconClick}/>
+      </div>
+    </div>
+  );
 }
 
 export default ParkingSpot;
