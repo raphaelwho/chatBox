1dd1f4b024dec3bc0191aeda257dab08a96db3ad
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-09-06 18:55:13 -0700

    refactored to dynamic lat/long values

---
+++ server/routes/controllers/index.js
@@ -0,0 +1,46 @@
+const { client } = require('../../../db');
+
+module.exports = {
+  findSpots: (req, res) => {
+      const { lat, long } = req.query;
+      const query = `SELECT jsonb_build_object(
+        'type',
+        'FeatureCollection',
+        'features',
+        jsonb_agg(feature)
+        )
+        FROM (
+          SELECT jsonb_build_object(
+                  'type',
+                  'Feature',
+                  'id',
+                  ogc_fid,
+                  'geometry',
+                  ST_AsGeoJSON(wkb_geometry)::jsonb,
+                  'properties',
+                  to_jsonb(row) - 'ogc_fid' - 'wkb_geometry'
+          ) AS feature
+          FROM (
+                  SELECT *,
+                      ST_Distance(
+                          ST_GEOGFromWKB(wkb_geometry),
+                          -- Los Angeles (LAX)
+                          ST_GEOGFromWKB(st_makepoint(${lat}, ${long}))
+                      ) as distance
+                  from austinrecycling
+                  order by distance
+                  limit 25
+              ) row
+          where distance < 16090
+      ) features`
+
+      client.query(query)
+        .then((results) => {
+          res.status(200).send(results);
+        })
+        .catch((err) => {
+          // console.log(err);
+          res.status(500).send(err);
+        })
+  }
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const profileRequests = require('./controllers/profileRequests');
-const spotSearches = require('./controllers/spotSearches');
+const controllers = require('./controllers');
 
 router.get('/profile', profileRequests.getProfile);
-
-router.get('/spots', spotSearches.getSpots);
+router.get('/spots', controllers.findSpots);
 
 module.exports = router;
