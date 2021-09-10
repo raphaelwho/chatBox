249fcbb41b2f891cb261321d6c01d511f67c614d
Jason Carr <carrjsn@gmail.com>
2021-09-09 20:18:39 -0700

    added controller/model files for spot management functions

---
+++ server/routes/controllers/manageSpots.js
@@ -0,0 +1,24 @@
+const model = require('../../models/manageSpots.js')
+
+const getMySpots = () => {
+
+}
+
+const addNewSpot = () => {
+
+}
+
+const getSpotDetails = () => {
+
+}
+
+const updateSpotDetails = () => {
+
+}
+
+module.exports = {
+  getMySpots,
+  addNewSpot,
+  getSpotDetails,
+  updateSpotDetails
+}
