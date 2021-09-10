28a55712d944706ee6c412e935ce9c17d693a0df
Jason Carr <carrjsn@gmail.com>
2021-09-10 16:04:14 -0700

    finished controllers for addSpot and updateSpotDetails

--- server/routes/controllers/manageSpots.js
+++ server/routes/controllers/manageSpots.js
@@ -5,6 +5,7 @@ const getMySpots = (req, res) => {
  model.getSpots(req.query.id)
    .then((spots) => {
      console.log('getting spots back to controller', spots);
+     res.status(200);
      res.send(spots)
    })
    .catch((err) => {
@@ -13,23 +14,40 @@ const getMySpots = (req, res) => {
 }
 
 const addNewSpot = (req, res) => {
- // req.body
+ // req.body = { hostId:   , lat:    , long:     , price:    , address:   , type:    , price:     , photo:    , }
+ model.addSpot(req.body)
+   .then(() => {
+     console.log('success adding new post - controller');
+     res.sendStatus(201);
+   })
+   .catch((err) => {
+     console.log('error posting new spot - controller', err);
+   })
 }
 
 const getSpotDetails = (req, res) => {
   // req.query.id = spotid
   model.getInfo(req.query.id)
    .then((info) => {
-     console.log('getting spots back to controller', info);
+     console.log('getting spots details to controller', info);
+     res.status(200);
      res.send(info);
    })
    .catch((err) => {
-     console.log('error getting spots from models');
+     console.log('error getting spot details from models', err);
    })
 }
 
 const updateSpotDetails = (req, res) => {
-  // req.body
+  // req.body = { photo:   , type:    , spotId:     , price:    }
+  model.updateInfo(req.body)
+    .then(() => {
+      console.log('success updating spot details - controller');
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log('error updating spot details - controller', err);
+    })
 
 }
 
