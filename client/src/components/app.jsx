fd756b487655c77fe64e86df56f1b21884a324d0
Huiqing Li <forqing@gmail.com>
2021-09-11 13:38:14 -0700

    refactor test children#

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -34,10 +34,10 @@ class App extends React.Component {
     return (
 
       <div>
-        {/* <PageHeader title={'Spot Management'} isVisible={true} />
+        <PageHeader title={'Spot Management'} isVisible={true} />
         <MapView />
         <Profile type={'registration'} />
-        <Profile type={'update'} /> */}
+        <Profile type={'update'} />
         <Login />
         {/* <TxtBox label={'yourLabel'} /> */}
       </div>
