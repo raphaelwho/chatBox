24c4effac2ec13bd778eaa0fd4c2df8bfc576511
raphaelwho <raphael-lhs@hotmail.com>
2021-09-11 12:54:44 -0500

    .

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -2,7 +2,7 @@ const client = require('../../db/index.js').client;
 
 exports.getProfile = async (post) => {
   try {
-    let query = `SELECT user_id,username,password, first_name,last_name ,email FROM users WHERE user_id =${post}`;
+    let query = `SELECT user_id,username,password, first_name,last_name ,email FROM users`;
     let ans = await client.query(query);
     return ans;
   } catch (error) {
