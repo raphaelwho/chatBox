f03f586d238c006406f6c36f37f0cd14decd5ffb
ahamilton454 <ahamilton454@gmail.com>
2021-09-11 02:54:19 -0400

    Minor CSS changes

--- client/src/components/shared/Picker.jsx
+++ client/src/components/shared/Picker.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Picker({label, options, initialValue, onChangeCB = () => {}}) {
+export default function Picker({ label, options, initialValue, onChangeCB = () => { } }) {
   const [selectedOption, setSelectedOption] = useState(initialValue);
 
   function handleSelectChange(event) {
@@ -15,12 +15,12 @@ export default function Picker({label, options, initialValue, onChangeCB = () =>
     <div className="Picker">
       <label>{label}</label>
       <select value={selectedOption} onChange={handleSelectChange}
-      style = {{
-                backgroundColor:'white',
-                borderColor: "#CDC4F2",
-                borderWidth: '5px',
-                borderRadius: '4px'
-      }}>
+        style={{
+          backgroundColor: 'white',
+          borderColor: "#CDC4F2",
+          borderWidth: '3px',
+          borderRadius: '4px'
+        }}>
         {options.map((option, index) => <option key={index} value={option}>{option}</option>)}
       </select>
     </div>
