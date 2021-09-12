ec4b8a7230ceb0bd186649c9bf3e9171ae97ae76
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-09-12 14:33:02 -0700

    added fake sf data

--- client/src/components/mapview/fakeSpots.js
+++ client/src/components/mapview/fakeSpots.js
@@ -38,5 +38,66 @@ export default [
         lng: 2.1915
       },
       address: 'fake address'
-    }
+    },
+    {
+      spot_id: 1000,
+      host_id: null,
+      address: '1329 42nd Ave. San Francisco, CA',
+      type: null,
+      price: '$15/hr',
+      photo_url: null,
+      location: {
+        lat: 37.761840,
+        lng: -122.502060
+      }
+    },
+    {
+      spot_id: 1001,
+      host_id: null,
+      address: '1329 45th Ave. San Francisco, CA',
+      type: null,
+      price: '$20/hr',
+      photo_url: null,
+      location: {
+        lat: 37.761700,
+        lng: -122.505270
+      }
+    },
+    {
+      spot_id: 1002,
+      host_id: null,
+      address: '1428 45th Ave. San Francisco, CA',
+      type: null,
+      price: '$18/hr',
+      photo_url: null,
+      location: {
+        lat: 37.758890,
+        lng: -122.504680
+      }
+    },
+    {
+      spot_id: 1003,
+      host_id: null,
+      address: '1628 41st Ave San Francisco, CA',
+      type: null,
+      price: '$14/hr',
+      photo_url: null,
+      location: {
+        lat: 37.756390,
+        lng: -122.500200
+      }
+    },
+    {
+      spot_id: 1004,
+      host_id: null,
+      address: '1601 44th Ave San Francisco, CA',
+      type: null,
+      price: '$11/hr',
+      photo_url: null,
+      location: {
+        lat: 37.756790,
+        lng: -122.503570
+      }
+    },
   ];
+
