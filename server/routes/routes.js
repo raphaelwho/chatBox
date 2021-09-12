db5bcfa37352e0a9814775253641e9ca759a6bda
raphaelwho <raphael-lhs@hotmail.com>
2021-09-11 20:58:50 -0500

    .

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const Profile = require('./controllers/Profile');
+const profileRequests = require('./controllers/profileRequests');
 const getSpotsRoute = require('./controllers/getSpots').getSpotsRoute;
 const {getUser} = require('./controllers/getUser');
 
+router.get('/profile', profileRequests.getProfile);
 router.get('/my-profile', Profile.getProfile);
+router.get('/profile', Profile.getProfile);
 router.post('/create-account',Profile.createProfile);
 router.get('/spots', getSpotsRoute);
 router.post('/login', getUser);
