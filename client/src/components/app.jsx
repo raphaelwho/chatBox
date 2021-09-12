68075bb3ea6c87af6e00251b967bb189e61aaf72
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-09-12 16:31:04 -0700

    uncommented out components

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -35,13 +35,13 @@ class App extends React.Component {
     return (
 
       <div>
-        {/* <PageHeader title={'Spot Management'} isVisible={true} /> */}
+        <PageHeader title={'Spot Management'} isVisible={true} />
         <MapView />
-        {/* <Profile type={'registration'} />
+        {/* <Profile type={'registration'} /> */}
         <Profile type={'update'} />
-        <Login /> */}
-        {/* <TxtBox label={'yourLabel'} /> */}
-        {/* <ManageSpots /> */}
+        <Login />
+        <TxtBox label={'yourLabel'} />
+        <ManageSpots />
       </div>
     )
   };
